87cb9885f991ca69569d16e9a7823d505d62822c
Quentin Vekeman-Marcelis <quentin.vekeman-marcelis@student.ehb.be>
2019-03-12 18:18:54 +0100

    Add project files.

---
+++ IP_Planning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IP_Planning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
